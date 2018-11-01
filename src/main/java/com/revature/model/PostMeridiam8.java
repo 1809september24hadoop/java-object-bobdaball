a38e1f50b4ea971f79262cb0f6c4c8c9547910c3
Alexander <amire51117@gmail.com>
2018-11-01 12:31:15 -0500

    class now extends and implements

--- src/main/java/com/revature/model/PostMeridiam8.java
+++ src/main/java/com/revature/model/PostMeridiam8.java
@@ -1,18 +1,35 @@
 package com.revature.model;
 
-public class PostMeridiam8 implements DinnerTime {
+public class PostMeridiam8 extends LeisureTime implements DinnerTime {
 	boolean isLate = false;
-	boolean isSatisfied = false;
-	boolean isHungry = true;
+	boolean hadFun = false;
+	String foodEaten = "delicious H2O";
 	
 	
 	//overloaded constructor 1
-	public PostMeridiam8 (boolean isLate, boolean isSatisfied, boolean isHungry) {
+	public PostMeridiam8 (String foodEaten, boolean hadFun) {
+		this.hadFun = hadFun;
+		this.foodEaten = foodEaten;
+	}
+	
+	public PostMeridiam8 (boolean isLate) {
 		this.isLate = isLate;
-		this.isSatisfied = isSatisfied;
-		this.isHungry = isHungry;
+		if (this.isLate == false) {
+			this.hadFun = true;
+			this.foodEaten = "the usual";
+		}
 	}
 	public static void main() {
+		
+	}
+	@Override
+	public void eatFood() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void playGames() {
+		// TODO Auto-generated method stub
 		
 	}
 }
